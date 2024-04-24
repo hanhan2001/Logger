77fdf400bdf288865afafd1a251bf693c985224a
Hanhan <764932129@qq.com>
2024-04-24 21:46:44 +0800

    feat: more customization and auto save when log file exists

--- src/main/java/me/xiaoying/logger/LoggerFactory.java
+++ src/main/java/me/xiaoying/logger/LoggerFactory.java
@@ -1,19 +1,45 @@
 package me.xiaoying.logger;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.zip.GZIPOutputStream;
 
-public class LoggerFactory {
-    File logPath = new File("./logs/latest.log");
+public abstract class LoggerFactory {
     private JNILogger jniLogger;
+    private File logFile = new File("./logs/latest.log");
+    private String defaultName = this.logFile.getParent() + "/" + new VariableFactory("%date%.log.gz").date("yyyy-MM-dd");
+    private String conflictName = this.logFile.getParent() + "/" + new VariableFactory("%date%-%i%.log.gz").date("yyyy-MM-dd");
 
     public LoggerFactory() {
         this.jniLogger = new JNILogger();
+
+        if (this.logFile.exists())
+            this.save();
+    }
+
+    public void setLogFile(String file) {
+        this.logFile = new File(file);
+    }
+
+    public void setLogFile(File file) {
+        this.logFile = file;
+    }
+
+    public String getDefaultName() {
+        return this.defaultName;
+    }
+
+    public void setDefaultName(String name) {
+        this.defaultName = name;
+    }
+
+    public String getConflictName() {
+        return this.conflictName;
+    }
+
+    public void setConflictName(String name) {
+        this.conflictName = name;
     }
 
     public Logger getLogger() {
@@ -27,14 +53,14 @@ public class LoggerFactory {
     public void log(String message) {
         message = ChatColor.stripColor(message);
 
-        if (!this.logPath.getParentFile().exists())
-            this.logPath.getParentFile().mkdirs();
+        if (!this.logFile.getParentFile().exists())
+            this.logFile.getParentFile().mkdirs();
 
         try {
-            if (!this.logPath.exists())
-                this.logPath.createNewFile();
+            if (!this.logFile.exists())
+                this.logFile.createNewFile();
 
-            BufferedWriter writer = new BufferedWriter(new FileWriter(this.logPath.getPath(), true));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(this.logFile.getPath(), true));
             writer.write(message + "\n");
             writer.close();
         } catch (IOException e) {
@@ -42,14 +68,14 @@ public class LoggerFactory {
         }
     }
 
-    public void saveLog() {
+    public void save() {
         try {
-            File file = new File(this.logPath.getParent() + "/" + new VariableFactory("%date%.log.gz").date("yyyy-MM-dd"));
+            File file = new File(new VariableFactory(this.defaultName).date("yyyy-MM-dd").toString());
             for (int i = 1; file.exists(); i++)
-                file = new File(this.logPath.getParent() + "/" + new VariableFactory("%date%-" + i + ".log.gz").date("yyyy-MM-dd"));
+                file = new File(new VariableFactory(this.conflictName).i(i).date("yyyy-MM-dd").toString());
 
             GZIPOutputStream gzipOutputStream = new GZIPOutputStream(new FileOutputStream(file));
-            FileInputStream fileInputStream = new FileInputStream(this.logPath);
+            FileInputStream fileInputStream = new FileInputStream(this.logFile);
             byte[] buf = new byte[1024];
             int len;
             while ((len = fileInputStream.read(buf)) > 0)
@@ -59,7 +85,7 @@ public class LoggerFactory {
             gzipOutputStream.finish();
             gzipOutputStream.close();
 
-            this.logPath.delete();
+            this.logFile.delete();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -74,6 +100,11 @@ public class LoggerFactory {
 
         public VariableFactory(String string) {
             this.string = string;
+        }
+
+        public VariableFactory i(int i) {
+            this.string = this.string.replace("%i%", String.valueOf(i));
+            return this;
         }
 
         public VariableFactory date(String format) {
